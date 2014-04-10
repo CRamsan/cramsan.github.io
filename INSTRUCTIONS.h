00d2d06f54bccbf5d590ee3528e412623ac9d264
Cesar Ramirez <cesar.ramsan@gmail.com>
2014-04-10 09:00:26 -0500

    Added a simple layout

--- INSTRUCTIONS.h
+++ INSTRUCTIONS.h
@@ -1,4 +1,4 @@
 # The instructions to set Jekyll are located at: https://help.github.com/articles/using-jekyll-with-pages
 # To run the server:
-echo "Once the server is running, go to http://localhost:4000"
-bundle exec jekyll serve
+echo "Once the server is running, go to http://localhost:4001"
+jekyll serve --watch
--- _posts/2014-04-08-Hello-World.md
+++
@@ -1,5 +0,0 @@
----
-layout: default
-title: Hello World
----
-Hello Again!
