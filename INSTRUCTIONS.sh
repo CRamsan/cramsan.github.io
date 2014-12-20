1ae9b864b9a541888cd4dff92fc97dfe6ba3b626
Cesar Ramirez <cesar.ramsan@gmail.com>
2014-12-20 01:39:11 -0800

    Improved html structure

--- INSTRUCTIONS.sh
+++ INSTRUCTIONS.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # The instructions to set Jekyll are located at: https://help.github.com/articles/using-jekyll-with-pages
 # To run the server:
-bundle exec jekyll serve --watch --trace
+bundle exec jekyll serve --trace 
