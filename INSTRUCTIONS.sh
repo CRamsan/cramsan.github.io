4fcf6ef6bbd10b10367e8627141d31d119178333
Cesar Ramirez <cesar.ramsan@gmail.com>
2014-12-20 00:46:55 -0800

    Fixed script to run server

--- INSTRUCTIONS.sh
+++ INSTRUCTIONS.sh
@@ -1,4 +1,4 @@
+#!/bin/bash
 # The instructions to set Jekyll are located at: https://help.github.com/articles/using-jekyll-with-pages
 # To run the server:
-echo "Once the server is running, go to http://localhost:4001"
-jekyll serve --watch --trace
+bundle exec jekyll serve --watch --trace
