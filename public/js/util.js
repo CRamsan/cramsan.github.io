8b2e0763961e0573b63b0090fcd7ab922a11be2d
Cesar Ramirez <cesar.ramsan@gmail.com>
2015-01-09 00:55:47 -0800

    Moved out of core-scalfold

--- public/js/util.js
+++ public/js/util.js
@@ -3,3 +3,11 @@ cramsanHelper = {
     setTimeout(function() { window.location = url; }, 500); 
   },
 };
+
+document.addEventListener('polymer-ready', function() {
+  var navicon = document.getElementById('navicon');
+  var drawerPanel = document.getElementById('drawerpanel');
+  navicon.addEventListener('click', function() {
+    drawerPanel.togglePanel();
+  });
+});
