039b8662dcaab84b203ec8f0748eb37412957c15
Cesar Ramirez <cesar.ramsan@gmail.com>
2016-08-04 03:39:34 +0000

    Added script for prod and debug   deployment

--- DEPLOY_DEBUG.sh
+++ DEPLOY_DEBUG.sh
@@ -2,4 +2,5 @@
 # The instructions to set Jekyll are located at: https://help.github.com/articles/using-jekyll-with-pages
 # To run the server:
 export PATH=~/.gem/ruby/2.2.0/bin:$PATH
-bundle exec jekyll serve --trace 
+bundle exec jekyll clean --destination _site/debug/
+bundle exec jekyll build --destination _site/debug/ --watch --trace
---
+++ DEPLOY_PROD.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# The instructions to set Jekyll are located at: https://help.github.com/articles/using-jekyll-with-pages
+# To run the server:
+export PATH=~/.gem/ruby/2.2.0/bin:$PATH
+bundle exec jekyll clean
+bundle exec jekyll build --trace 
