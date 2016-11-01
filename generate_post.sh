7c928ac3f5688fb0ce6bd21b6519ab8e144fd7d6
Cesar Ramirez <me@cramsan.com>
2016-10-31 19:13:43 -0700

    Adding script to generate post files

---
+++ generate_post.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+HELP_MESSAGE="generate_post.sh [POST TITLE]"
+
+if [ $# -eq 0 ]; then
+  echo $HELP_MESSAGE
+  exit
+fi
+
+if [ -z "$1" ]; then
+  echo "No argument supplied"  
+  exit
+fi
+
+DATE=`date +%Y-%m-%d`
+INPUT=$1
+TITLE="${INPUT//[^a-zA-Z0-9]/_}"
+NEW_FILE="_posts/$DATE-$TITLE.md"
+
+echo "File $NEW_FILE will be created"
+
+echo "---" > $NEW_FILE
+echo "layout: post" >> $NEW_FILE
+echo "title: "$POST_TITLE >> $NEW_FILE
+echo "---" >> $NEW_FILE
+echo >> $NEW_FILE
