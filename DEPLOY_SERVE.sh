5c05ccfd6bf6b77fb34ae554107e3ed1863b26fc
Cesar Ramirez <me@cramsan.com>
2017-02-25 11:16:37 -0800

    Improving build and deployment
    Adding a script to serve jekyll
    Also removing the requirement of an specific version of jekyll

--- DEPLOY_SERVE.sh
+++ DEPLOY_SERVE.sh
@@ -1,4 +1,3 @@
 # The instructions to set Jekyll are located at: https://help.github.com/articles/using-jekyll-with-pages
 # To run the server:
-bundle exec jekyll clean
-bundle exec jekyll build --trace --lsi
+bundle exec jekyll serve
