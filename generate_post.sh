0dcace9f241710c6a89de0dfa52b31b4d3bebb4b
Cesar Ramirez <me@cramsan.com>
2017-04-19 21:31:27 -0700

    Fixing missing games, cleaned up some files

--- generate_post.sh
+++ generate_post.sh
@@ -20,5 +20,7 @@ echo "File $NEW_FILE will be created"
 echo "---" > $NEW_FILE
 echo "layout: post" >> $NEW_FILE
 echo "title: $INPUT" >> $NEW_FILE
+echo "tags: random"
+echo "categories: other"
 echo "---" >> $NEW_FILE
 echo >> $NEW_FILE
