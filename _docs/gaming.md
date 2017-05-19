ae612827b1f97414054221defb79f0acfbdb34cf
Cesar Ramirez <me@cramsan.com>
2017-05-19 02:05:01 -0700

    Improving identation and filtering for collection index

--- _docs/gaming.md
+++ _docs/gaming.md
@@ -1,5 +1,5 @@
 ---
-title: Game Collection
+title: Gaming
 layout: page
 ---
 
--- generate_index.sh
+++ generate_index.sh
@@ -3,17 +3,16 @@
 
 function listFiles() {
   local ORIGINAL_FOLDER=$(pwd)
-  local TARGET_COLLECTION=$1
-  local TARGET_DEPTH=$2
-  local ROOT_FOLDER=$3
-  local ROOT_LABEL=$4
+  local TARGET_COLLECTION="$1"
+  local TARGET_DEPTH="$2"
+  local ROOT_FOLDER="$3"
+  local ROOT_LABEL="$4"
   local FIRST_ITER=true;
-  
   echo "{\"node\":\"$ROOT_LABEL\","
-  cd $TARGET_COLLECTION
   echo "\"level\": \"$TARGET_DEPTH\","
+  cd "$TARGET_COLLECTION"
   echo "\"list\": ["
-  find . -maxdepth 1 -name '*' | while read line; do
+  find . -maxdepth 1 -name '*' -printf '%h\0%p\n' | sort -t '\0' -n | awk -F '\0' '{print $2}' | while read line; do
     if [ "$line" = "." ]; then
       continue
     fi
@@ -22,17 +21,17 @@ function listFiles() {
     else
       echo ","
     fi
-    local FILE_PATH=$(dirname $line)
-    local FILE_BASENAME=$(basename $line)
+    local FILE_PATH=$(dirname "$line")
+    local FILE_BASENAME=$(basename "$line")
     local FILE_EXTENSION="${FILE_BASENAME##*.}"
     local FILE_FILENAME="${FILE_BASENAME%.*}"
-    local FILE_TITLE="$(grep "title:" $line | sed 's/title: //g')"
+    local FILE_TITLE="$(grep "title:" "$line" | sed 's/title: //g')"
     local NEW_DEPTH=$((TARGET_DEPTH+1))
     if [ -z "$FILE_TITLE" ]; then
       local FILE_TITLE="UNTITLED"
     fi
-    if [ -f $line ]; then
-      if [ ! -z $ROOT_FOLDER ]; then
+    if [ -f "$line" ]; then
+      if [ ! -z "$ROOT_FOLDER" ]; then
         local LINK_URL="$ROOT_FOLDER/"
       else
 	local LINK_URL=""
@@ -42,18 +41,17 @@ function listFiles() {
       echo "\"link\": \"$LINK_URL\","
       echo "\"name\": \"$FILE_TITLE\"}"
     else 
-      if [ -d $line ]; then
-        if [ ! -z $ROOT_FOLDER ]; then
+      if [ -d "$line" ]; then
+        if [ ! -z "$ROOT_FOLDER" ]; then
           local NEW_ROOT="$ROOT_FOLDER/$FILE_BASENAME"
         else
 	  local NEW_ROOT="$FILE_BASENAME"
         fi
-	listFiles $line $NEW_DEPTH $NEW_ROOT $FILE_BASENAME
+	listFiles "$line" "$NEW_DEPTH" "$NEW_ROOT" "$FILE_BASENAME"
       fi
     fi
   done
   echo "]}"
   cd $ORIGINAL_FOLDER
 }
-listFiles "_docs" 0 "" "docs" > _data/collection_docs.json
-
+listFiles "_docs" "0" "" "docs" > _data/collection_docs.json
