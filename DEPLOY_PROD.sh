80f18058aa3eb362291937cdbb76fc9d764bfa64
Cesar Ramirez <me@cramsan.com>
2017-06-14 00:43:14 -0700

    Cleaning up scripts

--- DEPLOY_PROD.sh
+++ DEPLOY_PROD.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # The instructions to set Jekyll are located at: https://help.github.com/articles/using-jekyll-with-pages
 # To run the server:
+python generate_flickrdb.py > _data/flickr.json
 ./generate_index.sh
 bundle exec jekyll clean --dest tmp_site
 bundle exec jekyll build --trace --dest tmp_site
