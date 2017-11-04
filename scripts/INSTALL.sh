e9fea735ef1789561a3174d0a9a35ef6c3e9597d
Cesar Ramirez <me@cramsan.com>
2017-11-03 22:53:31 -0700

    Improving INSTALL script

--- scripts/INSTALL.sh
+++ scripts/INSTALL.sh
@@ -4,6 +4,6 @@
 # 1. Install all the required gems and install jekyll and bundler.
 # 2. Use pip to install the flickerapi.
 export PATH=~/.gem/ruby/2.3.0/bin:$PATH
-gem update
-gem install jekyll bundler
+sudo gem update
+sudo gem install jekyll bundler
 pip install flickrapi  
