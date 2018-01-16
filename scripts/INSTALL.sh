9bb432adc26f3b8a1d345b74cb7cd644fe324789
Cesar Ramirez <me@cramsan.com>
2018-01-15 22:52:22 -0800

    Fixing paths for INSTALL and UPDATE scripts

--- scripts/INSTALL.sh
+++ scripts/INSTALL.sh
@@ -3,7 +3,7 @@
 # It is assumed that gem and pip are already installed.
 # 1. Install all the required gems and install jekyll and bundler.
 # 2. Use pip to install the flickerapi.
-export PATH=~/.gem/ruby/2.4.0/bin:$PATH
+export PATH=~/.gem/ruby/2.3.0/bin:~/.gem/ruby/2.4.0/bin:~/.gem/ruby/2.5.0/bin:$PATH
 gem update
 gem install jekyll bundler
 pip install flickrapi  
